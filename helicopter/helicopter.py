5585c21c785283a0c3ab0bea1161bc967ed238b2
haruyasu <harukun2002@gmail.com>
2017-10-29 12:42:13 -0700

    make block

--- helicopter/helicopter.py
+++ helicopter/helicopter.py
@@ -1,4 +1,5 @@
 import pygame, time
+from random import randint
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -8,10 +9,19 @@ pygame.init()
 surfaceWidth = 800
 surfaceHeight = 500
 
+imageHeight = 43
+imageWidth = 100
+
 surface = pygame.display.set_mode((surfaceWidth, surfaceHeight))
 pygame.display.set_caption('Helicopter')
 clock = pygame.time.Clock()
 img = pygame.image.load('Helicopter.png')
+
+def blocks(x_block, y_block, block_width, block_height, gap):
+    pygame.draw.rect(surface, white, [x_block, y_block, block_width, block_height])
+    pygame.draw.rect(surface, white, [x_block, y_block + block_height + gap, block_width, surfaceHeight])
+
+
 
 def replay_or_quit():
     for event in pygame.event.get([pygame.KEYDOWN, pygame.KEYUP, pygame.QUIT]):
@@ -58,6 +68,15 @@ def main():
     x = 150
     y = 200
     y_move = 4
+
+    x_block = surfaceWidth
+    y_block = 0
+
+    block_width = 75
+    block_height = randint(0, surfaceHeight / 2)
+    gap = imageHeight * 3
+    block_move = 6
+
     game_over = False
 
     while not game_over:
@@ -78,8 +97,15 @@ def main():
         surface.fill(black)
         helicopter(x, y, img)
 
+        blocks(x_block, y_block, block_width, block_height, gap)
+        x_block -= block_move
+
         if y > surfaceHeight - 40 or y < 0:
             gameOver()
+
+        if x_block <  (-1 * block_width):
+            x_block = surfaceWidth
+            block_height = randint(8, surfaceHeight / 2)
 
         pygame.display.update()
         clock.tick(60)
